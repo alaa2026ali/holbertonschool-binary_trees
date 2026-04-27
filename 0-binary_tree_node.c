cadb0615899a242ce0ea667967afb17f01faed26
MWA404 <muhannadalraddadi@gmail.com>
2026-04-27 10:57:34 +0300

    Fix binary_trees: Task 0 - Betty style fixes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node
- * @value: Value to store in the node
- *
- * Return: Pointer to the new node, or NULL on failure
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent,
+	int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *new_node;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (node);
+	return (new_node);
 }
