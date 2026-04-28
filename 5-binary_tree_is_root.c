9e32db0106ac857892bb3c9b58294d8c8ac0453b
alaa2026ali <ali_alaa18@icloud.com>
2026-04-28 10:24:55 +0300

    Fix Betty Style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,20 +5,14 @@
  * @node: pointer to the node to check
  *
  * Return: 1 if node is the root, 0 otherwise
- *
- * Description:
- * The root is the only node that has no parent.
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    /* Check if node is NULL */
     if (node == NULL)
         return (0);
 
-    /* Check if node has no parent */
     if (node->parent == NULL)
         return (1);
 
-    /* Otherwise, it is not the root */
     return (0);
 }
