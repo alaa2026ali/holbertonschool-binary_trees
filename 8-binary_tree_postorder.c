6cbdfc57ec413bad0a9e9710451d73a3f5a6f4fe
alaa2026ali <ali_alaa18@icloud.com>
2026-04-28 11:09:39 +0300

    Fix Betty Style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,7 +13,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
     /* Check if tree or function is NULL */
 if (tree == NULL || func == NULL)
- return;
+return;
 
     /* Traverse left subtree */
 binary_tree_postorder(tree->left, func);
