685e6216003d43a5f482444bd6e26f8c4786f1ea
alaa2026ali <ali_alaa18@icloud.com>
2026-04-28 06:54:52 +0000

    Add 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node
+ * @tree: pointer to the node
+ *
+ * Return: depth of the node, or 0 if NULL or root
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t depth = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    while (tree->parent != NULL)
+    {
+        depth++;
+        tree = tree->parent;
+    }
+
+    return (depth);
+}
