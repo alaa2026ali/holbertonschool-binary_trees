1093228defcf3889982e4c8739b7ac948618eddc
alaa2026ali <ali_alaa18@icloud.com>
2026-04-28 11:14:44 +0300

    Fix Betty Style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,22 +12,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height;
-    size_t right_height;
+size_t left_height;
+size_t right_height;
 
-    /* If tree is NULL, height is 0 */
-    if (tree == NULL)
-        return (0);
+ /* If tree is NULL, height is 0 */
+if (tree == NULL)
+return (0);
 
-    /* Recursively get height of left subtree */
-    left_height = binary_tree_height(tree->left);
-
-    /* Recursively get height of right subtree */
-    right_height = binary_tree_height(tree->right);
-
-    /* Return the larger height + 1 (current node) */
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+ /* Recursively get height of left subtree */
+left_height = binary_tree_height(tree->left);
+ /* Recursively get height of right subtree */
+right_height = binary_tree_height(tree->right);
+ /* Return the larger height + 1 (current node) */
+if (left_height > right_height)
+return (left_height + 1);
+else
+return (right_height + 1);
 }
